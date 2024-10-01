334d6317ba16cae332ec8dd763441f4c647e82d1
Abdelwahed <abdelwahedsouid@gmail.com>
2024-10-01 19:50:47 +0100

    elimine les composants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {SecteurActiviteComponent} from "./secteur-activite/secteur-activite.component";
-import {OperateurComponent} from "./operateur/operateur.component";
-import {FactureComponent} from "./facture/facture.component";
-import {ProductsComponent} from "./products/products.component";
-import {StockComponent} from "./stock/stock.component";
-import {ReglementComponent} from "./reglement/reglement.component";
+
 
 const routes: Routes = [
 
   {path: '', redirectTo: 'product', pathMatch: 'full'},
-  {path: 'secteurActivite', component: SecteurActiviteComponent},
-  {path: 'operateur', component: OperateurComponent},
-  {path: 'facture', component: FactureComponent},
- {path: 'product', component: ProductsComponent},
- {path: 'stock', component: StockComponent},
- {path: 'reglement', component: ReglementComponent},
+ // {path: 'secteurActivite', component: SecteurActiviteComponent},
+ // {path: 'operateur', component: OperateurComponent},
+ // {path: 'facture', component: FactureComponent},
+ //{path: 'product', component: ProductsComponent},
+ //{path: 'stock', component: StockComponent},
+ //{path: 'reglement', component: ReglementComponent},
 ]
 
 
