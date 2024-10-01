2de76a91889854aac0406f36ef4581748395359a
Abdelwahed <abdelwahedsouid@gmail.com>
2024-10-01 19:33:03 +0100

    ajouter les autres composants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SecteurActiviteComponent} from "./secteur-activite/secteur-activite.component";
+import {OperateurComponent} from "./operateur/operateur.component";
+import {FactureComponent} from "./facture/facture.component";
+import {ProductsComponent} from "./products/products.component";
+import {StockComponent} from "./stock/stock.component";
+import {ReglementComponent} from "./reglement/reglement.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'product', pathMatch: 'full'},
+  {path: 'secteurActivite', component: SecteurActiviteComponent},
+  {path: 'operateur', component: OperateurComponent},
+  {path: 'facture', component: FactureComponent},
+ {path: 'product', component: ProductsComponent},
+ {path: 'stock', component: StockComponent},
+ {path: 'reglement', component: ReglementComponent},
+]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {NgbModalModule, NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {ProductsComponent} from "./products/products.component";
+import {StockComponent} from "./stock/stock.component";
+import {ReglementComponent} from "./reglement/reglement.component";
+import {SecteurActiviteComponent} from "./secteur-activite/secteur-activite.component";
+import {OperateurComponent} from "./operateur/operateur.component";
+import {FactureComponent} from "./facture/facture.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    StockComponent,
+    ReglementComponent,
+    SecteurActiviteComponent,
+    OperateurComponent,
+    FactureComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModalModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
