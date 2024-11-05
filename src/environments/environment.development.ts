d3ab8a7e6b56858229f9ec8f8ce1933cf3525a1d
aymen-msalmi <aymen.msalmi@esprit.tn>
2024-11-05 02:02:18 +0100

    Refactor Dockerfile and add docker-compose for Angular app; update environment URLs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
 
-  url: 'http://localhost:8089/projet',
+  url: 'http://192.168.181.128:8089/projet',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  url: 'http://localhost:8089/projet',
+  url: 'http://192.168.181.128:8089/projet',
 };
 
 
